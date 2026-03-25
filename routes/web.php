1fb22e4f7398499e990dba78edc78ca5e6042f56
lokus233 <150285507+lokus233@users.noreply.github.com>
2026-03-25 18:10:12 +0100

    instalacion inertia (preguntar cosas)

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
-Route::inertia('/', 'welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
-])->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
 });
 
+Route::get('/', function () {return inertia('Home');
+});
+
 require __DIR__.'/settings.php';
+
+
+/*
+Route::inertia('/', 'welcome', [
+    'canRegister' => Features::enabled(Features::registration()),
+])->name('home');
+*/
